487211f8476adbf4aff5bb0b5c05902030412012
matthew.arvidson <matthew.arvidson@lightstream.com>
2019-05-24 10:17:23 -0700

    Working example

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   // selector: 'app-button',
   template: `
-    <p class="" [ngClass]="{test: true}">
-      button works!
+    <p class="" [ngClass]="{ test: test }">
+     ngClass works with
+     <code>
+     {{script1}}
+     </code>
+     <code>
+     {{script2}}
+     </code>
     </p>
   `,
+  styles: ['.test {font-size: 100px;}']
 })
 export class ButtonComponent implements OnInit {
-
-  constructor() { }
+  test = false;
+  script1 =
+  `  <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.2.10/custom-elements-es5-adapter.js"></script>`
+  script2 =`<script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.2.10/webcomponents-bundle.js"></script>`
+  constructor() {}
 
   ngOnInit() {
+    setInterval(() => {
+      this.test = !this.test;
+    }, 5000);
   }
-
 }
